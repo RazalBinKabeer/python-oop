4af43dde4e9418647ae128667ac21d069fa56bb8
Razal <razalkabeer@hotmail.com>
2023-05-21 12:02:37 +0400

    @classmethod, @staticmethod

--- oop.py
+++ oop.py
@@ -11,12 +11,25 @@ class PlayerCharacter:
     def run(self):
         print(f"{self.name} is Running")
         return 0
+    
+    # Class Method can be used without instanciating an object
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return cls("Tedddy", num1 + num2)
+    
+    # doesnt involve the class
+    @staticmethod
+    def adding_things2(num1, num2):
+        return num1 + num2
 
 
 player1 = PlayerCharacter("Razal", 22)
 player2 = PlayerCharacter("Rizin", 26)
 
-player1.run()
-player2.run()
-print(player1.age)
-print(player2.age)
+# player1.run()
+# player2.run()
+# print(player1.age)
+# print(player2.age)
+
+print(player1.adding_things(3, 5))
+print(PlayerCharacter.adding_things(3, 5))
